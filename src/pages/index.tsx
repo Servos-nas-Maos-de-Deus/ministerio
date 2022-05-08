82341522cf0797b4015e4e0fe12853445dd0b0a1
yuriBaza23 <yuribaza2321@gmail.com>
2022-05-08 20:52:28 -0300

    Initial page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { ApresentationSection } from '../components/Apresentation'
+
+const Home: NextPage = () => {
+  return (
+    <div className='h-screen'>
+      <ApresentationSection/>
+    </div>
+  )
+}
+
+export default Home
